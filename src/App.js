381ce465c4286bc3d57597fd5409e431bd1ad3b2
hienjoy <wisdom16no@naver.com>
2024-08-04 23:39:05 +0900

    add: 조언, 스탬프 경로 추가

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Login from "./pages/Login";
 import Join from "./pages/Join";
 import Home from "./pages/Home";
 import Pwd from "./pages/SetPassword";
-// import Stamp from "./pages/Stamp";
-// import Advice from "./pages/Advice";
+import Stamp from "./pages/Stamp";
+import Advice from "./pages/Advice";
 import { DateProvider } from "./DateContext";
 
 function App() {
@@ -17,8 +17,8 @@ function App() {
           <Route path="/join" element={<Join />} />
           <Route path="/settings/password" element={<Pwd />} />
           <Route path="/home" element={<Home />} />
-          {/* <Route path="/advice" element={<Advice />} />
-          <Route path="/stamp" element={<Stamp />} /> */}
+          <Route path="/advice" element={<Advice />} />
+          <Route path="/stamp" element={<Stamp />} />
         </Routes>
       </Router>
     </DateProvider>
