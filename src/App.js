d6664f21967ca6d744b6ed1302e19a7a240112df
hienjoy <wisdom16no@naver.com>
2024-08-04 11:33:20 +0900

    feat: 전역변수 date

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import Login from "./pages/Login";
 import Join from "./pages/Join";
 import Home from "./pages/Home";
 import Pwd from "./pages/SetPassword";
+// import Stamp from "./pages/Stamp";
+// import Advice from "./pages/Advice";
+import { DateProvider } from "./DateContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="/settings/password" element={<Pwd />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <DateProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/settings/password" element={<Pwd />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/advice" element={<Advice />} />
+          <Route path="/stamp" element={<Stamp />} /> */}
+        </Routes>
+      </Router>
+    </DateProvider>
   );
 }
 
