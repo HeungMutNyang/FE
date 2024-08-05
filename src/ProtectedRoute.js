584fa247da0d40d7f21c7130933aeb1991f435af
hienjoy <wisdom16no@naver.com>
2024-08-05 17:50:38 +0900

    fix: 새로고침 오류 수정

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -3,8 +3,10 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
 function ProtectedRoute({ element: Component, ...rest }) {
-  const { isAuthenticated } = useAuth();
-
+  const { isAuthenticated, loading } = useAuth();
+  if (loading) {
+    return <div>Loading...</div>; // 로딩 중
+  }
   return isAuthenticated ? Component : <Navigate to="/login" />;
 }
 
